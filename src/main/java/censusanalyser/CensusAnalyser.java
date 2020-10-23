c49044f9bccf565d04b3253b7d3c8bfc1619f058
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-23 12:10:42 +0530

    [Prerna]REFACTORED: Code to take list of data instead of iterating through file. Modified exceptions.

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -9,40 +9,58 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
-    public int loadIndiaCensusData(String csvFilePath) throws CSVException {
+    public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+        checkFileType(csvFilePath);
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createOpenCSVBuilder();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvBuilder.getCsvFileIterator(reader, IndiaCensusCSV.class);
-            return getCount(censusCSVIterator);
-        } catch (IOException | RuntimeException e) {
-            throw new CSVException(e.getMessage(),
-                    CSVException.ExceptionType.CSV_FILE_PROBLEM);
+            List<IndiaCensusCSV> censusCSVList = csvBuilder.getCsvFileList(reader, IndiaCensusCSV.class);
+            return censusCSVList.size();
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.HEADER_DELIMETER_PROBLEM);
+        } catch (CSVException e) {
+            throw new CensusAnalyserException(e.getMessage(), e.type.name());
         }
     }
 
-    public int loadIndianStateCode(String csvFilePath) throws CSVException {
+    public int loadIndianStateCode(String csvFilePath) throws CensusAnalyserException {
+        checkFileType(csvFilePath);
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createOpenCSVBuilder();
-            Iterator<IndiaStateCodeCSV> stateCSVIterator = csvBuilder.getCsvFileIterator(reader, IndiaStateCodeCSV.class);
-            return getCount(stateCSVIterator);
-        } catch (IOException | RuntimeException e) {
-            throw new CSVException(e.getMessage(),
-                    CSVException.ExceptionType.CSV_FILE_PROBLEM);
+            List<IndiaCensusCSV> stateCSVList = csvBuilder.getCsvFileList(reader, IndiaStateCodeCSV.class);
+            return stateCSVList.size();
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.HEADER_DELIMETER_PROBLEM);
+        } catch (CSVException e) {
+            throw new CensusAnalyserException(e.getMessage(), e.type.name());
         }
     }
 
-    public int loadIndiaCensusDataCommonsCSV(String csvFilePath) throws CSVException {
+    public int loadIndiaCensusDataCommonsCSV(String csvFilePath) throws CensusAnalyserException {
+        checkFileType(csvFilePath);
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCommonsCSVBuilder();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvBuilder.getCsvFileIterator(reader, IndiaCensusCSV.class);
-            return getCount(censusCSVIterator);
-        } catch (IOException | RuntimeException e) {
-            throw new CSVException(e.getMessage(),
-                    CSVException.ExceptionType.CSV_FILE_PROBLEM);
+            List<IndiaCensusCSV> censusCSVCommonsList = csvBuilder.getCsvFileList(reader, IndiaCensusCSV.class);
+            return censusCSVCommonsList.size();
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.HEADER_DELIMETER_PROBLEM);
+        } catch (CSVException e) {
+            throw new CensusAnalyserException(e.getMessage(), e.type.name());
         }
     }
 
@@ -52,7 +70,15 @@ public class CensusAnalyser {
         return numOfEnteries;
     }
 
-    public static void main(String[] args) {
+    private void checkFileType(String csvFilePath) throws CensusAnalyserException {
+        Pattern patternForCSV = Pattern.compile(".+[.csv]");
+        if (!patternForCSV.matcher(csvFilePath).matches())
+            throw new CensusAnalyserException("Incorrect file type",
+                    CensusAnalyserException.ExceptionType.FILE_TYPE_PROBLEM);
+    }
+
+    public void welcome(String[] args) {
         System.out.println("Welcome to Indian States Analyser Problem");
     }
+
 }
--- src/main/java/censusanalyser/CensusAnalyserException.java
+++ src/main/java/censusanalyser/CensusAnalyserException.java
@@ -1,17 +1,27 @@
 package censusanalyser;
 
+import csvbuilder.CSVException;
+
 public class CensusAnalyserException extends Exception {
 
     enum ExceptionType {
-        CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE;
+        CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE, HEADER_DELIMETER_PROBLEM, FILE_TYPE_PROBLEM;
     }
+
     ExceptionType type;
+
     public CensusAnalyserException(String message, ExceptionType type) {
         super(message);
         this.type = type;
     }
+
     public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
         super(message, cause);
         this.type = type;
+    }
+
+    public CensusAnalyserException(String message, String name) {
+        super(message);
+        this.type = ExceptionType.valueOf(name);
     }
 }
